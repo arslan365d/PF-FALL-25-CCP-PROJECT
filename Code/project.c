b8b1e4e61280db27d796762a771c24a84a4821a4
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-11-01 17:52:57 +0500

    Added the Code for blocking the Account if the Password Entered in Reverse Order

--- Code/project.c
+++ Code/project.c
@@ -12,11 +12,15 @@ struct User {
     char password[MAX_PASS];
     char email[MAX_EMAIL];
     int balance;
+    int isBlocked;
 };
 
 void stripNewline(char *str);
 int createYourAccount();
 int loginYourAccount(struct User *u);
+char *reversePassword(char password[MAX_PASS]);
+void printOptions(struct User *u);
+void balanceInquiry(struct User *u);
 
 int main() {
     int option;
@@ -29,8 +33,8 @@ int main() {
     printf("2 for Login\n");
     printf("Enter your Option: ");
     scanf("%d", &option);
-    while (getchar() != '\n'); 
-
+    while (getchar() != '\n'); // clear leftover newline
+ 
     switch (option) {
         case 1:
             createYourAccount();
@@ -78,11 +82,12 @@ int createYourAccount() {
     scanf("%d",&user.balance);
     stripNewline(user.password);
     
-    fprintf(file, "%s|%s|%s|%s|%d\n",
-            user.name, user.email, user.phone, user.password,user.balance);
+    fprintf(file, "%s|%s|%s|%s|%d|%d\n",
+            user.name, user.email, user.phone, user.password,user.balance,1);
 
     fclose(file);
     printf("\nAccount created successfully!\n");
+    printOptions(&user);
     return 0;
 }
 
@@ -108,25 +113,76 @@ int loginYourAccount(struct User *u) {
     int found = 0;
 
     while (fgets(line, sizeof(line), file)) {
-        if (sscanf(line, "%29[^|]|%24[^|]|%24[^|]|%31[^|]|%d",
-           u->name, u->email, u->phone, u->password, &u->balance) == 5) {
-
+        if (sscanf(line, "%29[^|]|%24[^|]|%24[^|]|%31[^|]|%d|%d",
+           u->name, u->email, u->phone, u->password, &u->balance,&u->isBlocked) == 6) {
+            if(u->isBlocked == 0){
             if (strcmp(loginName, u->name) == 0 && strcmp(loginPass, u->password) == 0) {
                 found = 1;
                 break;
-            }
+            }else if(strcmp(loginPass,reversePassword(u->password)) == 0){
+            	found = 7;
+            	break;
+			}
+		}
         }
     }
 
     fclose(file);
-
-    if (found) {
+   if(u->isBlocked == 0){
+   	 if (found==1) {
         printf("\nLogin successful!\n");
-        printf("Name: %s\nEmail: %s\nPhone: %s\nBalance: %d\n", u->name, u->email, u->phone,u->balance);
-    } else {
+        printOptions(u);
+    }else if(found == 7){
+            printf("\a Your Account is Blocked!!!");
+	} else {
         printf("\nLogin failed: wrong name or password.\n");
     }
+   }else{
+   	printf("Your Account is Blocked.\nPlease Visit our Main Branch");
+   }
+   
 
     return 0;
 }
+char *reversePassword(char password[MAX_PASS]){
+	int i;
+	char reversePass[MAX_PASS];
+	for(i=0;i<strlen(password);i++){
+		reversePass[i] = password[strlen(password)-i-1];
+	}
+	reversePass[strlen(password)] = '\0';
+	return reversePass;
+}
+
+void printOptions(struct User *u){
+	int transOption;
+	printf("\n\t\tPlease Select a Transaction\n\n");
+	printf("1 for BALANCE INQUIRY \t\t");
+	printf("2 for FAST CASH\n");
+	printf("3 for CASH WITHDRAWAL\t\t");
+	printf("4 for DEPOSIT\n\n");
+	printf("Choose any Option:");
+	scanf("%d",&transOption);
+	switch(transOption){
+		case 1:
+			balanceInquiry(u);
+			break;
+		case 2:
+//		    fastCash();
+			break;	
+		case 3:
+//		   	cashWithdrawal();
+		   	break;
+		case 4:
+//		    deposit();
+			break;  
+		default:
+		   printf("Please Select Valid Operation!");	 	
+	}
+}
+
+void balanceInquiry(struct User *u){
+	 printf("Name: %s\nEmail: %s\nPhone: %s\nBalance: %d\n", u->name, u->email, u->phone,u->balance);
+}
+
 
